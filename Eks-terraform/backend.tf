b35d5ef5a9c7715b148e06b884d160b9526dadfe
pinaflo <102924052+pinaflo@users.noreply.github.com>
2025-01-08 13:42:24 +0000

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "vprofile-pinaflo-artifacts" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
